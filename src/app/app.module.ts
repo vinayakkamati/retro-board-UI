3da708bb19897778073bc4355f42c3d85c584742
Vinayak Kamati <ervinayakkamati@gmail.com>
2023-08-02 22:27:32 +0530

    [IF-220] feature: create login and signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { FormsModule } from '@angular/forms';
 
+const appRoutes: Routes = [
+  {path:'', component: LogInComponent},
+  {path:'register', component: RegistrationComponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    RegistrationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/user-dto.interface.ts
@@ -0,0 +1,5 @@
+export interface UserDTO{
+    id: bigint;
+    userName: string;
+    emailId: string;
+}
