14c18dd8019c78648c43fc32d3f5121e85c6541f
Vinayak Kamati <119689320+vinayakkamati@users.noreply.github.com>
2023-08-02 22:31:34 +0530

    [IF-220] feature: create login and signup page (#1)

---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { UserDTO } from 'src/app/models/user-dto.interface';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent {
+ 
+  createUser(user: UserDTO): void{
+      
+  }
+}
